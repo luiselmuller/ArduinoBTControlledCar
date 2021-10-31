983474700887df01037b63364ba64ed1eb0250ec
luiselmuller <luiselmuller@gmail.com>
2021-10-30 23:11:36 -0400

    Changed the control structure from if to switch

--- DualHbridgeControl.ino
+++ DualHbridgeControl.ino
@@ -6,18 +6,16 @@
  * 
  * How To Use:
  * Byte = 3 -> Forward
- * Byte = 1 -> Turn right with wheels turning forward
- * Byte = 2 -> Turn left with wheels turning forward
+ * Byte = 1/7 -> Turn right 
+ * Byte = 2/6 -> Turn left 
  * Byte = 8 -> Reverse
- * Byte = 5 -> Turn left with wheels turning backward
- * Byte = 4 -> Turn right with wheels turning forward
+ * Byte = 5/6 -> Turn left 
+ * Byte = 4/7 -> Turn right 
  * Byte = 9 -> Stop
  * 
+ * === Connect BTModule RX pin to TX pin on arduino ===
+ * ===   and BTModule TX pin to RX pin on arduino   ===
  *****************************************************/
- 
-//#include<SoftwareSerial.h>
-//SoftwareSerial Blue(2, 3); //RX | TX
-
 //pins used to turn motors on/off
 int forwardRight = 5;  //moves the motors on the right side forward
 int forwardLeft = 9;  //moves the motors on the left side forward
@@ -30,39 +28,27 @@ int spd = 255;
 void setup() 
 {
   // put your setup code here, to run once:
-
   pinMode(forwardRight, OUTPUT);
   pinMode(forwardLeft, OUTPUT);
   pinMode(backwardRight, OUTPUT);
   pinMode(backwardLeft, OUTPUT);
   
   Serial.begin(9600);
-  //serial communication with bluetooth module
-  //Blue.begin(9600);
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly
-  //read data from arduino to send to bluetooth module 
-  //if(Serial.available())
-  //{
-    //char data = Serial.readString();
-    //Blue.println(data);
-  //}
-  
   if(Serial.available() > 0) //if there is data to read
   {
     Serial.println("**BLUETOOTH CONNECTED**");
     char rByte = Serial.read();  //reads the data from the bluetooth module
     
     //Button controls
-    if(rByte == 'b')
+    switch(rByte)
     {
-        //if the user inputs 1 the backwards pins are turned off and the forward pins are turned on with a
+      //if the user inputs 1 the backwards pins are turned off and the forward pins are turned on with a
       //small delay in between
-      if(rByte == '3')
-      {
+      case '3':
         Serial.println("Moving Forward");
         //forward
         analogWrite(backwardRight, 0);
@@ -70,24 +56,20 @@ void loop()
         delay(500);
         analogWrite(forwardRight, spd);
         analogWrite(forwardLeft, spd);
-        
-      }
+      break;
       //if the user inputs 2 the forward pins are turned off and the backwards pins are turned on with a 
       //small delay in between
-      if(rByte == '8')
-      {
+      case '8':
         Serial.println("Moving Backward");
-      //backward
+        //backward
         analogWrite(forwardRight, 0);
         analogWrite(forwardLeft, 0);
         delay(500);
         analogWrite(backwardRight, spd);
         analogWrite(backwardLeft, spd);
-        
-      }
+      break;
       //if the user inputs 3 the right side forward pin is turned to spdpwm
-      if(rByte == '2')
-      {
+      case '2':
         Serial.println("Left");
         //turns left
         analogWrite(backwardRight, 0);
@@ -95,11 +77,9 @@ void loop()
         analogWrite(backwardLeft, 0);
         delay(500);
         analogWrite(forwardRight, spd);
-        
-      }
+      break;
       //if the user inputs 4 the right side forward pin is turned to spdpwm
-      if(rByte == '1')
-      {
+      case '1':
         Serial.println("Right");
         //turns right
         analogWrite(forwardRight, 0);
@@ -107,48 +87,49 @@ void loop()
         analogWrite(backwardRight, 0);
         delay(500);
         analogWrite(forwardLeft, spd);
-  
-      }
+      break;
+      //turns left by turning the left motors in reverse and right motors forward
+      case '6':
+        analogWrite(forwardLeft, 0);
+        analogWrite(forwardRight, 0);
+        delay(500);
+        analogWrite(backwardLeft, spd);
+        analogWrite(forwardRight, spd);
+      break;
+      //turns right by turning the left motors forward and right motors in reverse
+      case '7':
+        analogWrite(forwardLeft, 0);
+        analogWrite(forwardRight, 0);
+        delay(500);
+        analogWrite(forwardLeft, spd);
+        analogWrite(backwardRight, spd);
+      break;
       //turns left using reverse on one side
-      if(rByte == '5')
-      {
-        Serial.println("Moving Backward Left");
-      //left
+      case '5':
+        //left
         analogWrite(forwardRight, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardRight, 0);
         delay(500);
         analogWrite(backwardLeft, spd);
-      }
+      break;
       //turns right using reverse on one side
-      if(rByte == '4')
-      {
-        Serial.println("Moving Backward Right");
-      //right
+      case '4':
+        //right
         analogWrite(forwardRight, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardLeft, 0);
         delay(500);
         analogWrite(backwardRight, spd);
-      }
+      break;
       //if the user inputs 0 the car will stop
-      if(rByte == '9')
-      {
+      case '9':
         //stops
         analogWrite(forwardRight, 0);
         analogWrite(backwardLeft, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardRight, 0);
         delay(500);
-  
-      }
-    }
-    //joystick
-    if(rByte == 'a')
-    {
-      
-    }
+   }
   }
-
-
 }
