f6752876d6ce84ffaec205e9d9003b5bf61571af
luiselmuller <luiselmuller@gmail.com>
2021-11-06 21:29:02 -0400

    Added temperature/humidity sensor
    Added code to send temperature and humidity readings from dht11 sensor to app

--- DualHbridgeControl.ino
+++ DualHbridgeControl.ino
@@ -1,4 +1,4 @@
-/******************************************************
+/**********************************************************************
  * Luisel A. Muller Rodriguez
  * 9/25/2021
  * Program to control 4 motors using 2 h-bridge
@@ -12,12 +12,19 @@
  * Byte = 5/6 -> Turn left 
  * Byte = 4/7 -> Turn right 
  * Byte = 9   -> Stop
- * Byte = d   -> Speed is lowered to a minimum of 160pwm
- * Byte = u   -> Speed is increased to a maximum of 255pwm
+ * Byte = d   -> Speed is lowered 5pwm at a time, minimum of 160pwm
+ * Byte = u   -> Speed is increased 5pwm at a time, maximum of 255pwm
  * 
  * === Connect BTModule RX pin to TX pin on arduino ===
  * ===   and BTModule TX pin to RX pin on arduino   ===
- *****************************************************/
+ *********************************************************************/
+#include <dht_nonblocking.h>
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+
+static const int DHT_SENSOR_PIN = 2;
+DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
+
+
 //pins used to turn motors on/off
 int forwardRight = 5;  //moves the motors on the right side forward
 int forwardLeft = 9;  //moves the motors on the left side forward
@@ -35,14 +42,34 @@ void setup()
   pinMode(backwardRight, OUTPUT);
   pinMode(backwardLeft, OUTPUT);
   
-  Serial.begin(9600);
+  Serial.begin(115200);
+}
+
+/*
+ * Poll for a measurement, keeping the state machine alive.  Returns
+ * true if a measurement is available.
+ */
+static bool measure_environment( float *temperature, float *humidity )
+{
+  static unsigned long measurement_timestamp = millis( );
+
+  /* Measure once every four seconds. */
+  if( millis( ) - measurement_timestamp > 3000ul )
+  {
+    if( dht_sensor.measure( temperature, humidity ) == true )
+    {
+      measurement_timestamp = millis( );
+      return( true );
+    }
+  }
+
+  return( false );
 }
 
 void loop() 
 {
-  if(Serial.available() > 0) //if there is data to read
+  if(Serial.available()) //if there is data to read
   {
-    Serial.println(spd);
     char rByte = Serial.read();  //reads the data from the bluetooth module
     //Button controls
     switch(rByte)
@@ -56,82 +83,79 @@ void loop()
         if(spd <= 250)
           spd += 5;
       break;
+      
       //if the user inputs 3 the backwards pins are turned off and the forward pins are turned on with a
       //small delay in between
       case '3':
-        Serial.println("Moving Forward");
         //forward
         analogWrite(backwardRight, 0);
         analogWrite(backwardLeft, 0);
-        delay(200);
         analogWrite(forwardRight, spd);
         analogWrite(forwardLeft, spd);
       break;
+      
       //if the user inputs 8 the forward pins are turned off and the backwards pins are turned on with a 
       //small delay in between
       case '8':
-        Serial.println("Moving Backward");
         //backward
         analogWrite(forwardRight, 0);
         analogWrite(forwardLeft, 0);
-        delay(200);
         analogWrite(backwardRight, spd);
         analogWrite(backwardLeft, spd);
       break;
+      
       //if the user inputs 2 the right side forward pin is turned to spdpwm
       case '2':
-        Serial.println("Left");
         //turns left
         analogWrite(backwardRight, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardLeft, 0);
-        //delay(200);
         analogWrite(forwardRight, spd);
       break;
+      
       //if the user inputs 1 the right side forward pin is turned to spdpwm
       case '1':
-        Serial.println("Right");
         //turns right
         analogWrite(forwardRight, 0);
         analogWrite(backwardLeft, 0);
         analogWrite(backwardRight, 0);
-        //delay(200);
         analogWrite(forwardLeft, spd);
       break;
+      
       //turns left by turning the left motors in reverse and right motors forward
       case '6':
         analogWrite(forwardLeft, 0);
         analogWrite(forwardRight, 0);
-       // delay(200);
         analogWrite(backwardLeft, spd);
         analogWrite(forwardRight, spd);
       break;
+      
       //turns right by turning the left motors forward and right motors in reverse
       case '7':
         analogWrite(forwardLeft, 0);
         analogWrite(forwardRight, 0);
-        //delay(200);
         analogWrite(forwardLeft, spd);
         analogWrite(backwardRight, spd);
       break;
+      
       //turns left using reverse on one side
       case '5':
         //left
         analogWrite(forwardRight, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardRight, 0);
-        //delay(200);
         analogWrite(backwardLeft, spd);
       break;
+      
       //turns right using reverse on one side
       case '4':
         //right
         analogWrite(forwardRight, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardLeft, 0);
-        //delay(200);
         analogWrite(backwardRight, spd);
       break;
+      
       //if the user inputs 0 the car will stop
       case '9':
         //stops
@@ -139,7 +163,23 @@ void loop()
         analogWrite(backwardLeft, 0);
         analogWrite(forwardLeft, 0);
         analogWrite(backwardRight, 0);
-        delay(200);
+      break;
    }
   }
+  float temperature;
+  float humidity;
+
+  //temp/hum measurement
+  if( measure_environment( &temperature, &humidity ) == true )
+  {
+    //temperature conversion from Celsius to Fahrenheit
+    float temp = (temperature * 9/5) + 32;
+    
+    //sending data to app
+    String out = "";
+    out.concat(temp);
+    out.concat("|");
+    out.concat(humidity);
+    Serial.println(out);
+   }
 }
